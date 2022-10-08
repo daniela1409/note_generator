d7e28763993f39adadcaf77c787bedc54e026634
daniela1409 <ydaniela.osorio@udea.edu.co>
2022-10-07 21:13:02 -0500

    first commit

---
+++ src/main/java/com/note_generator/app/NoteGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.note_generator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/note_generator/app/NoteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note_generator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
